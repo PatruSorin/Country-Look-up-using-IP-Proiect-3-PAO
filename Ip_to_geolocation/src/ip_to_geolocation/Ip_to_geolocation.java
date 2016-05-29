07f5e633b52be1cab42df0432ffac45b80049528
Patru Sorin <patrusorin95@gmail.com>
2016-05-29 15:55:00 +0300

    Proiect 3 terminat

---
+++ Ip_to_geolocation/src/ip_to_geolocation/Ip_to_geolocation.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ip_to_geolocation;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Patru Sorin
+ */
+public class Ip_to_geolocation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Interfata form =new Interfata();
+        form.setVisible(true);
+        
+        
+        
+         
+        
+        
+        
+        
+        
+    }
+    
+}
